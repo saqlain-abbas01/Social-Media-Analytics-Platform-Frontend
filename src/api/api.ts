f1b332e77de58c3cf416149a16bbba8c1ab1b8e2
saqlain <saqlain.abbas@codethix.com>
2025-11-09 02:08:20 +0500

    Updated api function

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useAuthStore } from "@/store/useAuthStore";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
